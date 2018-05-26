0aabc386569f5203374feb1115b2674c1943597f
gamingdark <excgames@gmail.com>
2018-05-26 12:06:02 +0300

    V1.03
    Main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ExC Test Tasks
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/todo') }}">Task #1: To-Do List</a>
+                    <a href="{{ url('/weather') }}">Task #2: Weather</a>
                 </div>
             </div>
         </div>
